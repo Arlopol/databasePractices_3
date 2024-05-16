99f227147c424db387f26c896a656b6c7a7a2cfe
Arlopol <125224000+Arlopol@users.noreply.github.com>
2024-05-16 16:10:03 +0300

    a

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -77,6 +77,39 @@ namespace WindowsFormsApp1
             {
                 comboBox1.Items.Add(combo_read[0]);
             }
+           
+            connection.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand lecture_command = new OleDbCommand("select ders_id from tbl_ders where ders_adı='" + comboBox1.SelectedItem.ToString() + "'", connection);
+            OleDbDataReader lecture_read = lecture_command.ExecuteReader();
+            lecture_read.Read();
+            int lecture_id = lecture_read.GetInt32(0);
+
+            OleDbCommand grade_command = new OleDbCommand("select * from tbl_not where ders_id=" + lecture_id, connection);
+            OleDbDataReader grade_read = grade_command.ExecuteReader();
+            while (grade_read.Read())
+            {
+                int student_no = grade_read.GetInt32(1);
+                string visa = grade_read.GetInt32(3).ToString();
+                string final = grade_read.GetInt32(4).ToString();
+                string letter = grade_read.GetString(5);
+
+                //listBox2.Items.Add(student_no + " " + visa + " " + final + " " + letter);
+
+                OleDbCommand student_command = new OleDbCommand("select adısoyadı from tbl_öğrenci where öğrenci_no=" +student_no, connection);
+                OleDbDataReader student_read = student_command.ExecuteReader();
+                student_read.Read();
+
+                string nameLastname = student_read[0].ToString();
+
+                listBox2.Items.Add(nameLastname + " " + visa + " " + final + " " + letter);
+            }
+
+            listBox2.Items.Add(lecture_id);
             connection.Close();
         }
     }
